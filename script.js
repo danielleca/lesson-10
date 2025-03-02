4e199cd9603f3cd52b98789aa54b49d7a94bc48b
danielleca <shuwen.campbell@icloud.com>
2025-03-02 13:58:38 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+class Movie{
+    constructor(title, director, actor, year){
+        this.title=title
+        this.director=director
+        this.actor=actor
+        this.year=year
+
+    }
+
+}
+const defaultm=[
+    {
+        title:'Frozen',
+        director:'Chris Buck',
+        actor:'Elsa',
+        year:'2013',
+    }
+]
